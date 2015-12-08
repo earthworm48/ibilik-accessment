3dbf999b760acea513e646b439ab373a3249bda9
Sam <samkahchiin@gmail.com>
2015-12-08 09:46:03 +0800

    log in form works

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -10,5 +10,21 @@ post '/users' do
 end
 
 get "/users/:user_id" do
+	@user = User.find(params[:user_id])
 	erb :"users/profile"
+end
+
+post '/users/login' do
+	@user = User.find_by(email: params[:email]) 
+	if @user.nil?
+		@error = "No such email address"
+		erb :"static/error"
+	elsif @user.authenticate(params[:password])	
+		session[:user_id] = @user.id
+		redirect "/users/#{session[:user_id]}"
+	else
+		@error = "Wrong combination of e-mail and password"
+		erb :"static/error"
+	end
+
 end
